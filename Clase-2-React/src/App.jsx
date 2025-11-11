725d467be6548993eaf68febf2978b27376a48de
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-11 09:53:53 -0300

    Clase 2

---
+++ Clase-2-React/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginForm from './Components/LoginForm/LoginForm'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <LoginForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ Clase-2-React/src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const LOGIN_FORM_NAME_FIELDS = {
+    EMAIL: 'email',
+    PASSWORD: 'password'
+}
+
+const LoginForm = () => {
+
+  
+    // Extraer la info de los campos y guardarla en un estado
+    // Este estado debe tener en tiempo real los datos que hayan en cada campo
+
+    const initial_form_state = {
+        [LOGIN_FORM_NAME_FIELDS.EMAIL]: '',
+        [LOGIN_FORM_NAME_FIELDS.PASSWORD]: ''
+    }
+
+    const [form_state, setFormState] = useState(
+        initial_form_state
+    )
+
+    function handleChange (event){
+        console.log('hola')
+    } 
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="email">Ingresa tu email</label>
+                <br/>
+                <input 
+                    type='email' 
+                    name={LOGIN_FORM_NAME_FIELDS.EMAIL} 
+                    id="email" 
+                    onChange={handleChange}
+                    value={form_state[LOGIN_FORM_NAME_FIELDS.EMAIL]}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Ingresa tu contraseña</label>
+                <br/>
+                <input 
+                    type='password' 
+                    name={LOGIN_FORM_NAME_FIELDS.PASSWORD} 
+                    id="password" 
+                    onChange={handleChange}
+                    value={form_state[LOGIN_FORM_NAME_FIELDS.PASSWORD]}
+                />
+            </div>
+            <br/>
+            <button type='submit'>Iniciar sesion</button>
+        </form>
+    )
+}
+
+export default LoginForm
