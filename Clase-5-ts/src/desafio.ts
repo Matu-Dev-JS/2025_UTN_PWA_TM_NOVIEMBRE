c7d7b7a769f35535d2c3f75c518487cbf06bb36d
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-20 10:13:35 -0300

    Desafio clase 5-ts

---
+++ Clase-5-ts/src/desafio.ts
@@ -0,0 +1,19 @@
+/* 
+Crear la class Item
+    -nombre
+    -precio
+    -id
+    -descripcion
+    -Mostrar item: Mostrara por consola el mensaje: {nombre} es {descripcion} y cuesta ${precio} Rupias
+
+ItemTienda extends Item
+    -stock
+    -margen_ganancia
+    -setStock(nuevo_stock) y va a guardar el nuevo stock como valor de stock
+
+Crear la class Tienda
+    -nombre
+    -cantidad_dinero_en_cuenta
+    -items: ItemTienda[],
+    -id
+ */
