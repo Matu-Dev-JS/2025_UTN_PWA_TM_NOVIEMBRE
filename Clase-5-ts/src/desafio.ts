d4e59ad832f396ea8c0f75fd4c44cf37f081b5bd
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-20 10:31:56 -0300

    Clase 5

--- Clase-5-ts/src/desafio.ts
+++ Clase-5-ts/src/desafio.ts
@@ -16,4 +16,23 @@ Crear la class Tienda
     -cantidad_dinero_en_cuenta
     -items: ItemTienda[],
     -id
+    -buscarItemPorId(id_item_buscado) 
+    -buscar en la lista de items el item buscado y devolvera el mismo, sino devolvera null
+
+    -eliminarPorId(id_item_buscado): true (si elmina) | false (si no elimina)
+        -eliminar el item con el id recibido y devolver un boolean de status
+
+    -modificarStockItem(id_item_buscado, cantidad)
+        -checkear que la cantidad a decrementar sea menor o igual que el stock del item con el id buscado
+        -Si es menor decrementar el stock del item utilizando setStock
+
+    -vender(id_item_a_vender cantidad_vendida) 
+        -Buscar el item en la lista de items, si existe lo va a decrementar usando modificarStockItem y incrementara su cantidad de dinero por la (cantidad_vendida * precio * margen_ganancia)
+        -Si no hay item con ese id lanzar mensaje de error "Producto no encontrado"
+        -Si el stock del item es 0 lanzar mensaje de error "Producto agotado"
+    
+    -comprar(nuevo_item: Item, cantidad: number, margen_gancia: number)
+        -Calcular el costo de la operacion y en caso de ser mayor a la cantidad de dinero en cuenta lanzar un error por consola diciendo, no se puede realizar la operacion, motivo: no hay dinero suficiente
+        -Crear un ItemTienda a partir de Item
+        -pushear a la lista de items
  */
