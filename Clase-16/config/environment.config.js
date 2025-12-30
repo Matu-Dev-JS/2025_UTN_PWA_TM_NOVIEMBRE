591427919ffce87cd4931d6facdf3a13683b4dc8
matu.dev <mati.a.gimenez2002@gmail.com>
2025-12-30 09:54:38 -0300

    Clase 15

---
+++ Clase-16/config/environment.config.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv"
+/* Esto carga las variables de entorno del archivo .env en process.env */
+dotenv.config()
+
+const ENVIRONMENT = {
+    MONGO_DB_URI: process.env.MONGO_DB_URI,
+    MONGO_DB_NAME: process.env.MONGO_DB_NAME,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+}
+
+export default ENVIRONMENT
---
+++ Clase-16/controllers/auth.controller.js
@@ -0,0 +1,95 @@
+import ENVIRONMENT from "../config/environment.config.js"
+import userRepository from "../repository/user.repository.js"
+import jwt from 'jsonwebtoken'
+
+class AuthController {
+    async register (request, response) {
+
+        try{     
+
+            const {email, password, username} = request.body
+            if(!email || !password || !username){
+                return response.send('Error: Nombre, email o usuario invalido')
+            }
+
+            const user = await userRepository.buscarUnoPorEmail(email)
+            if(user){
+                return response.send(`Usuario con email ${email} ya registrado`)
+            }
+    
+            await userRepository.crear(email, password, username)
+    
+            return response.send('Usuario creado exitosamente')
+        }
+        catch(error){
+            console.error('Error al crear usuario:', error)
+
+            return response.send('Error interno del servidor')
+        }
+    }
+
+    async login (request, response){
+        const {email, password} = request.body
+        /* 
+        Aplicar validaciones sobre el email y la password
+        */
+        if(!email){
+            return response.json({
+                ok: false,
+                message: 'Debes enviar un email',
+                status: 400,
+                data: null
+            })
+        }
+        else if(!(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email))){
+            return response.json({
+                ok: false,
+                status: 400,
+                message: 'El email no es valido',
+                data: null
+            })
+        }
+
+        const usuario_encontrado = await userRepository.buscarUnoPorEmail(email)
+
+        if(usuario_encontrado.password !== password){
+            return response.send(
+                {
+                    message: 'Contraseña incorrecta', 
+                    status: 401, 
+                    ok: false,
+                    data: null
+                }
+            )
+        }
+
+        const datos_del_token = {
+            username: usuario_encontrado.username,
+            email: usuario_encontrado.email,
+            id: usuario_encontrado.id
+        }
+
+        const auth_token = jwt.sign(datos_del_token, ENVIRONMENT.JWT_SECRET_KEY)
+        return response.json({
+            message: 'Inicio de sesion exitoso',
+            ok: true,
+            status: 200,
+            data: {
+                auth_token: auth_token
+            }
+        })
+    }
+}
+
+const authController = new AuthController()
+export default authController
+
+
+
+/* 
+condicion sobre una variable
+un bucle de 100 a 1000 registros (donde no se consulta a ningun servicio externo)
+una consulta a DB (Debatible porque si la DB tiene millones de registros puede ser mas costoso)
+una consulta a otro servidor
+*/
+
