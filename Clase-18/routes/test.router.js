80b41038635fa37fe293ba8f815e68928cc630b7
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-08 10:30:12 -0300

    Test de auth

--- Clase-18/routes/test.router.js
+++ Clase-18/routes/test.router.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import testController from '../controllers/test.controller.js'
+import authMiddleware from '../middlewares/auth.middlware.js'
 
 const testRouter = express.Router()
 
@@ -7,6 +8,22 @@ const testRouter = express.Router()
 testRouter.get(
     '/', 
     testController.get
+)
+
+testRouter.get(
+    '/authorized-test',
+    authMiddleware,
+    (request, response) => {
+        console.log({user_data: request.user})
+        return response.json(
+            {
+                ok: true, 
+                status: 200,
+                message: 'Test correcto',
+                data: null
+            }
+        )
+    }
 )
 
 export default testRouter
