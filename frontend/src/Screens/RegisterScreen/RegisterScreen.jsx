fbad2b7481be5129de80b9500ca6979750dfaabc
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-08 12:00:05 -0300

    useForm

--- frontend/src/Screens/RegisterScreen/RegisterScreen.jsx
+++ frontend/src/Screens/RegisterScreen/RegisterScreen.jsx
@@ -1,22 +1,62 @@
 import React from 'react'
 import { Link } from 'react-router'
+import useForm from '../../hooks/useForm'
 
 const RegisterScreen = () => {
+    /* Que campos existe en mi formulario y que valor tienen */
+    const form_initial_state = {
+        username: '',
+        password: '',
+        email: ''
+    }
+
+    function enviarRegistro (form_state){
+        console.log({form_state})
+    }
+
+    const {
+        form_state,
+        onChangeFieldValue,
+        onSubmitForm
+    } = useForm(
+        {
+            initial_form_fields: form_initial_state,
+            onSubmit: enviarRegistro
+        }
+    )
   return (
     <div>
         <h1>Registrate en la aplicacion</h1>
-        <form>
+        <form onSubmit={onSubmitForm}>
             <div>
                 <label htmlFor="username">Nombre de usuario:</label>
-                <input type="text" id="username" name="username" />
+                <input 
+                    type="text" 
+                    id="username" 
+                    name="username" 
+                    value={form_state.username} 
+                    onChange={onChangeFieldValue} 
+                />
             </div>
             <div>
                 <label htmlFor="password">Contraseña:</label>
-                <input type="password" id="password" name="password" />
+                <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    value={form_state.password} 
+                    onChange={onChangeFieldValue} 
+                />
             </div>
             <div>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
+                <input 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    value={form_state.email} 
+                    onChange={onChangeFieldValue}
+                />
             </div>
             <button type="submit">Registrarse</button>
         </form>
