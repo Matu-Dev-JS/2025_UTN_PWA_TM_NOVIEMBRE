e50cbe2b4bbfbfd8db61284de955fdeb5d477f5d
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-15 10:12:19 -0300

    AuthMiddleware

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import LoginScreen from './Screens/LoginScreen/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen/RegisterScreen'
 import AuthContextProvider from './context/AuthContext'
+import AuthMiddleware from './Middlewares/AuthMiddleware'
 
 function App() {
 
@@ -13,6 +14,9 @@ function App() {
         <Route path='/' element={<LoginScreen />} />
         <Route  path='/register' element={<RegisterScreen />} />
         <Route  path='/login' element={<LoginScreen />} />
+        <Route element={<AuthMiddleware />}>
+          <Route path='/home' element={<h1>Home</h1>} />
+        </Route>
       </Routes>
     </AuthContextProvider>
   )
--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext()
@@ -7,8 +7,23 @@ export const AUTH_TOKEN_KEY = 'auth_token'
 
 function AuthContextProvider ({children}){
     const [isLogged, setIsLogged] = useState(false)
-    const [session, setSession] = useState({})
+    const [session, setSession] = useState(null)
 
+    useEffect(
+        () => {
+            /* 
+            Normalmente los backends suelen tener un endpoint 
+            GET /api/auth/validate-token (Authorization: Bearer auth_token) Te responde si el token es valido o no
+            */
+            const auth_token = localStorage.getItem(AUTH_TOKEN_KEY)
+            if(auth_token){
+                setIsLogged(true)
+                const session_decoded = jwtDecode(auth_token)
+                setSession(session_decoded)
+            }
+        }, 
+        []
+    )
 
     function saveSession (auth_token){
         localStorage.setItem(AUTH_TOKEN_KEY, auth_token)
